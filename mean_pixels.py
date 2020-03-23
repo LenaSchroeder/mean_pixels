ee1a0e988ec6b0ccfe08b8bfe7a203dceb93b456
Lena <lena.schroeder@tum.de>
2020-03-23 17:04:17 +0100

    added function description

--- mean_pixels.py
+++ mean_pixels.py
@@ -3,6 +3,17 @@ import statistics
 
 
 def calculate_pixel_brightness(x, y, z):
+    """
+    Calculates the brightness of a pixel based on its r, g, b values.
+
+    Args:
+        x (int): the red value of the pixel.
+        y (int): the green value of the pixel.
+        z (int): the blue value of the pixel.
+
+    Returns: the brightness of the pixel
+
+    """
     return statistics.mean([x, y, z])
 
 
@@ -14,3 +25,4 @@ pixels = [
 pixel_brightnesses = [calculate_pixel_brightness(pixel['red'], pixel['green'], pixel['blue']) for pixel in pixels]
 mean_brightness = statistics.mean(pixel_brightnesses)
 print(mean_brightness)
+
