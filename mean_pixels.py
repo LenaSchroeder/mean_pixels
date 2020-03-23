8d113ae97d9ca33ba2c2f6e9cf92b9ee22858c3c
Lena <lena.schroeder@tum.de>
2020-03-23 16:20:24 +0100

    DRY (don't repeat yourself)

--- mean_pixels.py
+++ mean_pixels.py
@@ -1,14 +1,19 @@
 # This is my project
 
+
+def calculate_pixel_brightness(x, y, z):
+    brightness = (red + green + blue) / 3
+    return brightness
+
+
 red, green, blue = 100, 0, 0
-pixel_brightness = (red + green + blue) / 3
+pixel_brightness = calculate_pixel_brightness(red, green, blue)
 
 red2, green2, blue2 = 50, 100, 0
-pixel_brightness2 = (red2 + green2 + blue2) / 3
+pixel_brightness2 = calculate_pixel_brightness(red2, green2, blue2)
 
 readiness = 0
 readiness2 = 200
 
 mean_brightness = (pixel_brightness + pixel_brightness2) / 2
 print(mean_brightness)
-
